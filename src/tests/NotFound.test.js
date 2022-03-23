66b3213c1a115a3a00163757d1a8df5854d12229
SheilaNS <shei.nsantos@gmail.com>
2022-03-22 22:55:51 -0300

    Requisito 04 - ok

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import NotFound from '../components/NotFound';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testa o componente NotFound.js', () => {
+  it('Verifica se a página tem um h2 com o texto "Page requested not found"', () => {
+    renderWithRouter(<NotFound />);
+
+    const text = screen.getByRole('heading',
+      { name: /Page requested not found/i, level: 2 });
+    expect(text).toBeInTheDocument();
+  });
+
+  it('Verifica se a página tem uma imagem com o caminho "https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif"', () => {
+    renderWithRouter(<NotFound />);
+    const URL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    const image = screen.getByRole('img',
+      { name: 'Pikachu crying because the page requested was not found' });
+    expect(image.src).toBe(URL);
+  });
+});
